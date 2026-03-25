dfb2a21669a9add113f28ac556c7250de270bed7
PabloHPaul <avezado81@hotmail.com>
2026-03-25 11:57:15 -0300

    medicacion

--- pages/efectos.py
+++ pages/efectos.py
@@ -47,9 +47,6 @@ def render():
     # st.divider()
     # col_a, col_b = st.columns(2, gap="medium")
 
-    st.divider()
-    col_a = st.columns(2, gap="medium")
-
     with col_a:
         st.markdown("#### Otros efectos adversos")
         st.session_state["ef_otros"] = st.text_area(
